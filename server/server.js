ab88b057c759874f2cecf111e861e39495815d5c
ARCHAN GIRMOHANTA <ankushbhanja8@gmail.com>
2025-03-14 17:26:53 +0530

    Updated changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
@@ -22,7 +22,6 @@ app.use(cors({
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/payouts', require('./routes/payouts'));
-app.use('/api/conversions', require('./routes/conversions'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -31,9 +30,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
